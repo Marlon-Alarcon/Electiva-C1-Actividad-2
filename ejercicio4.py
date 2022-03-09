77daed11a3d5a0f91bccaf0b1384cd82175eacf8
Marlon-Alarcon <mralarcon@uniguajira.edu.co>
2022-03-08 20:39:05 -0500

    Creacion de menu

--- ejercicio4.py
+++ ejercicio4.py
@@ -1,6 +1,6 @@
 from api.library import *
 
-def main():
+def Forma1():
 
     persona = {
         "nombre" : "Marlon",
@@ -10,6 +10,34 @@ def main():
 
     print(persona["nombre"]+" " + persona["apellido"]+" "+persona["edad"])
 
+def menu():
+    men = 0
+    while men != 3:
+        print("")
+        print("   MENÚ   ")
+        print("1. Forma 1")
+        print("2. Forma 2")
+        print("3. Salir")
+        print("")
+        men = int(input("Ingrese una opcion por favor "))
+
+        if men == 1:
+            Forma1()
+        
+        elif men == 2:
+            print("")
+            print("hola opcion 2")
+
+        elif men == 3:
+            print("")
+            print("-----   Has salido   -----")
+            print("***** Muchas Gracias *****")
+        
+        else:
+            print("")
+            print(" -----  Opcion no valida  -----")
+            print(" ----- Intente Nuevamente -----")
+
 
 if __name__ == "__main__":
-    main()
+    menu()
