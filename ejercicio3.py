5a2b9f2792da6d6146929b2f21bb29cbb2f7ce92
acastellon1 <100424690+acastellon1@users.noreply.github.com>
2022-03-10 17:49:38 -0500

    Corrección

--- ejercicio3.py
+++ ejercicio3.py
@@ -12,3 +12,19 @@ print("Hola mundo")
 *************************************************
 
 """
+
+def  listas():
+     lista1 = []
+     lista2 = list()
+
+     ListaConElementos = [30, 2000000, "Andres Castellon", "Personal de tienda", True, ["Bachiller", "Pregrado", "Tecnico", 20]]
+     print("")
+     print(ListaConElementos)
+
+
+def main():
+    listas()
+
+
+if __name__=="__main__":
+    main()          
