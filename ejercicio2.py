c861654ee88f28af81c3b14f3391b4715c9944be
ACAMARGO97 <100289673+ACAMARGO97@users.noreply.github.com>
2022-03-10 17:39:19 -0500

    cambio 2

--- ejercicio2.py
+++ ejercicio2.py
@@ -5,3 +5,11 @@ from api.library import *
 def main():
     SALARIO_MIN = 1000000
     SUB_ALIM = 120000
+    SUB_TRANS = 80000
+    BONO = 50000
+    nombre = input("Digite el nombre: ")
+    salario = int(input("Digite el salario mensual: "))
+    diastrabajados = int(input("Digite los dias trabajados: "))
+    sueldopagar = calcularsueldo(salario, diastrabajados)
+
+    
