06508a2dcfacb8b4fdeca7ee9b2720878744db90
acastellon1 <100424690+acastellon1@users.noreply.github.com>
2022-03-10 15:46:04 -0500

    Utilizando ciclo while

--- ejercicio3.py
+++ ejercicio3.py
@@ -14,12 +14,30 @@ def  listas():
      lista1 = []
      lista2 = list()
 
-     ListaConElementos = [30, 2000000, "Ing sistemas" ,"Andres Castellon", "Personal de tienda", True, ["Bachiller", "Pregrado", "Tecnico", 20]]
+     ListaConElementos = [30, 2000000, "Andres Castellon", "Personal de tienda", True, ["Bachiller", "Pregrado", "Tecnico", 20]]
 
      #print(ListaConElementos)
 
+     # Utilizando ciclo for
+
+     print("Mostrando elementos con ciclo for :)")
+
      for i in range(len(ListaConElementos)):
          print(ListaConElementos[i])
+
+     # Utilizando ciclo while
+
+     print("")
+     print("")
+     print("")
+     print("Mostrando elementos con ciclo while :)")
+
+     j=0
+     while j < len(ListaConElementos):
+         print(ListaConElementos[j])
+         #j = j + 1
+         j+= 1
+
 
 
 def main():
